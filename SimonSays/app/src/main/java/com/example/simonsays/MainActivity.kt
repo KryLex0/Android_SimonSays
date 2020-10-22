3f5ce977a5c846237443c4b02ff19d83cddf4702
Adam M <adam.mkh@hotmail.fr>
2020-10-22 21:02:50 +0200

    Réglage du problème d'animation sur les boutons. Les animations des boutons fonctionnent maintenant correctement / Remplacement du nom de la nouvelle couleur par un bouton de couleur

--- SimonSays/app/src/main/java/com/example/simonsays/MainActivity.kt
+++ SimonSays/app/src/main/java/com/example/simonsays/MainActivity.kt
@@ -2,30 +2,38 @@ package com.example.simonsays
 
 import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
 
 
 class MainActivity : AppCompatActivity() {
     var temp: Int = 0
-    val tabColor = arrayOf("rouge", "blue", "vert", "jaune")
+    val tabColorName = arrayOf("RED", "BLUE", "GREEN", "YELLOW")
+    val tabColor = arrayOf("#FF0000", "#4169E1", "#008000", "#FFFF00")
     var gagne = true
     private lateinit var val_score: TextView
-    private lateinit var tv_color: TextView
+    private lateinit var tv_color_name: TextView
+    private lateinit var tv_score : TextView
+    private lateinit var tv_val_score : TextView
 
     private lateinit var btn_1: Button
     private lateinit var btn_2: Button
     private lateinit var btn_3: Button
     private lateinit var btn_4: Button
     private lateinit var btn_start: Button
+    private lateinit var btn_color: Button
+
     private var tab: MutableList<Button> = mutableListOf()//tab = arrayOf<Button>()
 
     private var listColorParty: MutableList<Button> = mutableListOf() //= arrayOf<Button>()
@@ -37,12 +45,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val_score = findViewById(R.id.tv_val_score)
-        tv_color = findViewById(R.id.tv_color)
+        tv_color_name = findViewById(R.id.tv_color_name)
         btn_1 = findViewById(R.id.btn_1)
         btn_2 = findViewById(R.id.btn_2)
         btn_3 = findViewById(R.id.btn_3)
         btn_4 = findViewById(R.id.btn_4)
         btn_start = findViewById(R.id.btn_start)
+        btn_color = findViewById(R.id.btn_color)
+
+        tv_score = findViewById(R.id.tv_score)
+        tv_val_score = findViewById(R.id.tv_val_score)
 
         tab.addAll(listOf(btn_1, btn_2, btn_3, btn_4))
 
@@ -53,6 +65,10 @@ class MainActivity : AppCompatActivity() {
             btn_2.setVisibility(View.VISIBLE)
             btn_3.setVisibility(View.VISIBLE)
             btn_4.setVisibility(View.VISIBLE)
+            tv_color_name.setVisibility(View.VISIBLE)
+            btn_color.setVisibility(View.VISIBLE)
+            tv_score.setVisibility(View.VISIBLE)
+            tv_val_score.setVisibility(View.VISIBLE)
         }
 
         btn_1.setOnClickListener{
@@ -85,45 +101,58 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun animButton(btn: Button, varTemp: Long){
+    private fun animButton(btn: MutableList<Button>, index: Int){
         val anim: Animation = AlphaAnimation(1.0f, 0.2f)
-        anim.duration = 500
-        anim.startOffset = varTemp
+        anim.duration = 250
+        anim.startOffset = 500
+        anim.interpolator = LinearInterpolator()
         anim.repeatMode = Animation.REVERSE
 
-        btn.startAnimation(anim)
+        btn[index].startAnimation(anim)
 
-    }
+            anim.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationRepeat(animation: Animation?) {}
+                override fun onAnimationStart(animation: Animation?) {}
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    val index1 = index + 1
+                    if(index < btn.size - 1) {
+                        animButton(btn, index1)
+                    }
+                }
+            })
+        }
 
     private fun nextLevel(){
         temp = (0..3).random()
-        var varTemp:Long = 1000
-        tv_color.text = tabColor[temp]
+        val index = 0
+        //tv_color.text = tabColor[temp]
+        btn_color.text = tabColorName[temp]
+        btn_color.setBackgroundColor(Color.parseColor(tabColor[temp]))
         listColorParty.add(tab[temp])
 
-        if(this.gagne) {
-            listColorParty.forEach{
-                Handler().postDelayed({
-                    animButton(it, varTemp)
-                    varTemp += 1000
-                }, 1000)
-            }
+        tab.forEach {
+            it.isClickable = false
         }
+
+        if(this.gagne) {
+            animButton(listColorParty, index)
+        }
+
+        Handler().postDelayed({
+            tab.forEach {
+                it.isClickable = true
+            }
+        }, (500 * (listColorParty.size+1)).toLong())
     }
 
 
     @SuppressLint("SetTextI18n")
-    private fun verifCouleur(){//listColorPlayer : MutableList<String>){
-        //for(item in listColorParty) {
+    private fun verifCouleur(){
         var i = 0
-//        Log.d("test", listColorParty.size.toString())
-//        Log.d("test", listColorParty[0].id.toString())
-//        Log.d("test", listColorPlayer.size.toString())
-//        Log.d("test", listColorPlayer[0])
-
         while(i<listColorParty.size){
 
-            if(listColorParty[i].id.toString() == listColorPlayer[i]){//if(it.id.toString() == idBtnClick) {//if(numBtn == idBtnClick){
+            if(listColorParty[i].id.toString() == listColorPlayer[i]){
                 Log.d("test", "gagné")
                 gagne = true
             } else {
@@ -148,6 +177,7 @@ class MainActivity : AppCompatActivity() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Partie terminé")
         builder.setMessage("Votre score est de " + val_score.text)
+        builder.setCancelable(false)
 
         builder.setPositiveButton(
             "Ok"
@@ -155,7 +185,6 @@ class MainActivity : AppCompatActivity() {
             val intent = intent
             finish()
             startActivity(intent)
-            //Toast.makeText(this@MainActivity, "Alert dialog closed.", Toast.LENGTH_LONG).show()
         }
         builder.show()
     }
